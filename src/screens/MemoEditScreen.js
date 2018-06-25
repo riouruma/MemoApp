676c3e66ae9a1afcae1044c9bcc00e0c4c1f1e9a
riouruma <rio.uruma@gmail.com>
2018-06-25 18:03:40 +0900

    create EditMemoScreen

---
+++ src/screens/MemoEditScreen.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { StyleSheet, View, TextInput } from 'react-native';
+
+import CircleButton from '../elements/CircleButton';
+
+class MemoEditScreen extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <TextInput style={styles.memoEditInput} multiline value="Hi" />
+
+        <CircleButton>
+          {'\uf00c'}
+        </CircleButton>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    width: '100%',
+  },
+  memoEditInput: {
+    backgroundColor: '#ddd',
+    paddingTop: 32,
+    paddingLeft: 16,
+    paddingRight: 16,
+    paddingBottom: 16,
+    flex: 1,
+    fontSize: 16,
+  },
+});
+
+export default MemoEditScreen;
