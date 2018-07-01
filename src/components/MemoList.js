76fb134b5b80823a37db920c2cd00e5419283449
riouruma <rio.uruma@gmail.com>
2018-07-01 16:21:19 +0900

    add delete fuction

--- src/components/MemoList.js
+++ src/components/MemoList.js
@@ -1,27 +1,67 @@
 import React from 'react';
 import { StyleSheet, View, Text, TouchableHighlight, FlatList } from 'react-native';
+import firebase from 'firebase';
+
+import Swipeable from 'react-native-swipeable';
 
 const dateString = (date) => {
   const str = date.toDate().toLocaleDateString();
   return str.split('/').join('-');
 };
 
+const handlePressDelete = (docId) => {
+  const { currentUser } = firebase.auth();
+  const db = firebase.firestore();
+  const settings = { timestampsInSnapshots: true };
+  db.settings(settings);
+  db.collection(`users/${currentUser.uid}/memos`).doc(docId).delete()
+    .then()
+    .catch();
+};
+
+const rightButtons = (key) => {
+  return [
+    <TouchableHighlight
+      style={{
+        backgroundColor: 'red',
+        flex: 1,
+        justifyContent: 'center',
+      }}
+      onPress={() => { handlePressDelete(key); }}
+    >
+      <Text
+        style={{
+          color: '#fff',
+          marginLeft: 15,
+        }}
+      >
+        Delete
+      </Text>
+    </TouchableHighlight>,
+  ];
+};
+
 class MemoList extends React.Component {
   renderMemo({ item }) {
     return (
-      <TouchableHighlight onPress={() => { this.props.navigation.navigate('MemoDetail', { memo: item }); }}>
-        <View style={styles.memoListItem}>
-          <Text style={styles.memoTitle}>{item.body.substring(0, 15)}</Text>
-          <Text style={styles.memoDate}>{dateString(item.created_at)}</Text>
-        </View>
-      </TouchableHighlight>
+      <Swipeable rightButtons={rightButtons(item.key)}>
+        <TouchableHighlight onPress={() => { this.props.navigation.navigate('MemoDetail', { memo: item }); }}>
+          <View style={styles.memoListItem}>
+            <Text style={styles.memoTitle}>{item.body.substring(0, 15)}</Text>
+            <Text style={styles.memoDate}>{dateString(item.created_at)}</Text>
+          </View>
+        </TouchableHighlight>
+      </Swipeable>
     );
   }
 
   render() {
     return (
       <View style={styles.memoList}>
-        <FlatList data={this.props.memoList} renderItem={this.renderMemo.bind(this)} />
+        <FlatList
+          data={this.props.memoList}
+          renderItem={this.renderMemo.bind(this)}
+        />
       </View>
     );
   }
@@ -46,6 +86,12 @@ const styles = StyleSheet.create({
     borderBottomWidth: 1,
     borderBottomColor: '#ddd',
     backgroundColor: '#fff',
+  },
+  deleteView: {
+    backgroundColor: 'red',
+  },
+  deleteText: {
+    color: '#fff',
   },
 });
 
